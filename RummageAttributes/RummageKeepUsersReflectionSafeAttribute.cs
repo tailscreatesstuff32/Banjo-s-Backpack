c5df5c9b0c3a4f22689b26c18a99054eadc23128
banjobackpack <67706681+banjobackpack@users.noreply.github.com>
2020-07-01 12:29:13 -0700

    Add files via upload

---
+++ RummageAttributes/RummageKeepUsersReflectionSafeAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: RummageAttributes.RummageKeepUsersReflectionSafeAttribute
+// Assembly: BanjoKazooieLevelEditor, Version=2.0.19.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9E4E8A9F-6E2F-4B24-B56C-5C2BF24BF813
+// Assembly location: C:\Users\runem\Desktop\BanjosBackpack\BB.exe
+
+using System;
+
+namespace RummageAttributes
+{
+  [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+  public sealed class RummageKeepUsersReflectionSafeAttribute : Attribute
+  {
+  }
+}
